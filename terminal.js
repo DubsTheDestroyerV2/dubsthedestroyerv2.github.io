37edd0539b824c7b64d0e1ec2767ab90535986c2
DubsTheDestroyerV2 <55600331+DubsTheDestroyerV2@users.noreply.github.com>
2019-11-06 08:56:15 -0500

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -1,4 +1,6 @@
 var can = document.getElementById('screen');
+can.width = document.documentElement.clientWidth;
+can.height = document.documentElement.clientHeight;
 var ctx = can.getContext('2d');
 for (var i = 4; i < can.clientWidth; i += 4) {
   ctx.beginPath();
