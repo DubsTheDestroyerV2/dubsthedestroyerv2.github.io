cd589eac09c7ec9e14350d75091a97a11f81f67a
DubsTheDestroyerV2 <55600331+DubsTheDestroyerV2@users.noreply.github.com>
2019-11-06 09:07:25 -0500

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -2,15 +2,16 @@ var can = document.getElementById('screen');
 can.width = document.documentElement.clientWidth;
 can.height = document.documentElement.clientHeight;
 var ctx = can.getContext('2d');
-for (var i = 4; i < can.clientWidth; i += 4) {
+var i;
+for (i = 4; i < can.clientWidth; i += 4) {
   ctx.beginPath();
   ctx.moveTo(i, 0);
   ctx.lineTo(i, can.clientHeight);
   ctx.stroke();
-};
-for (var i = 4; i < can.clientHeight; i += 4) {
+}
+for (i = 4; i < can.clientHeight; i += 4) {
   ctx.beginPath();
   ctx.moveTo(0, i);
   ctx.lineTo(can.clientWidth, i);
   ctx.stroke();
-};
+}
