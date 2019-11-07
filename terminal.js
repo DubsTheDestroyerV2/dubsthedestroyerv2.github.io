95ac2baad89303487d9583ffdb5cdbf10e30e6d5
DubsTheDestroyerV2 <55600331+DubsTheDestroyerV2@users.noreply.github.com>
2019-11-07 08:46:50 -0500

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -1,19 +1,23 @@
 var can = document.querySelector(".overlay");
 can = can.querySelector(".screen");
-can.width = window.innerWidth;
-can.height = window.innerHeight;
-console.log(can.width + ' vs ' + can.height);
-var ctx = can.getContext('2d');
-var i;
-for (i = 4; i < can.clientWidth; i += 4) {
-  ctx.beginPath();
-  ctx.moveTo(i, 0);
-  ctx.lineTo(i, can.clientHeight);
-  ctx.stroke();
+function makeCRT() {
+  can.width = window.innerWidth;
+  can.height = window.innerHeight;
+  console.log(can.width + ' vs ' + can.height);
+  var ctx = can.getContext('2d');
+  var i;
+  for (i = 4; i < can.clientWidth; i += 4) {
+    ctx.beginPath();
+    ctx.moveTo(i, 0);
+    ctx.lineTo(i, can.clientHeight);
+    ctx.stroke();
+  }
+  for (i = 4; i < can.clientHeight; i += 4) {
+    ctx.beginPath();
+    ctx.moveTo(0, i);
+    ctx.lineTo(can.clientWidth, i);
+    ctx.stroke();
+  }
 }
-for (i = 4; i < can.clientHeight; i += 4) {
-  ctx.beginPath();
-  ctx.moveTo(0, i);
-  ctx.lineTo(can.clientWidth, i);
-  ctx.stroke();
-}
+makeCRT();
+window.addEventListener("resize", makeCRT());
