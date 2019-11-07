13f250cbc647b14213aea8e4934819c503c7513b
DubsTheDestroyerV2 <55600331+DubsTheDestroyerV2@users.noreply.github.com>
2019-11-07 09:02:42 -0500

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -5,15 +5,15 @@ function makeCRT() {
   can.height = window.innerHeight;
   console.log(can.width + ' vs ' + can.height);
   var ctx = can.getContext('2d');
-  ctx.lineWidth = 1;
+  ctx.lineWidth = 0.1;
   var i;
-  for (i = 8; i < can.clientWidth; i += 8) {
+  for (i = .5; i < can.clientWidth; i += .5) {
     ctx.beginPath();
     ctx.moveTo(i, 0);
     ctx.lineTo(i, can.clientHeight);
     ctx.stroke();
   }
-  for (i = 8; i < can.clientHeight; i += 8) {
+  for (i = .5; i < can.clientHeight; i += .5) {
     ctx.beginPath();
     ctx.moveTo(0, i);
     ctx.lineTo(can.clientWidth, i);
