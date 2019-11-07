230a67f14f46b8d36412cd98c0589d330f5483d9
DubsTheDestroyerV2 <55600331+DubsTheDestroyerV2@users.noreply.github.com>
2019-11-07 10:44:53 -0500

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -28,9 +28,9 @@ window.addEventListener("resize", makeCRT);
 var all = document.documentElement;
 function flicker() {
   if(all.style.opacity > 0.25) {
-    all.style.opacity -= 0.05;
+    all.style.opacity -= 0.1;
     return;
   }
-  all.style.opacity += 0.05;
+  all.style.opacity += 0.1;
 }
 setInterval(flicker, 0.001);
