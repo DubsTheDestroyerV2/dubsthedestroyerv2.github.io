ac38899ca16a0abdf5f929dd3bf04e2f5b5e5149
DubsTheDestroyerV2 <55600331+DubsTheDestroyerV2@users.noreply.github.com>
2019-11-12 18:42:33 -0800

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -9,7 +9,7 @@ window.onload = function() {
     console.log(can.width + ' vs ' + can.height);
     var ctx = can.getContext('2d');
     ctx.strokeStyle = '#000000';
-    ctx.lineWidth = 0.2;
+    ctx.lineWidth = 0.4;
     var i;
     for (i = 3; i < can.clientWidth; i += 3) {
       ctx.beginPath();
@@ -39,5 +39,17 @@ window.onload = function() {
   }
   setInterval(flicker, 1);
   
-   
+  var light = document.getElementById('light'); 
+  var bootseq = "INIT";
+  light.innerHTML = bootseq;
+  var i = -1;
+  function setTxt() {
+    i++;
+    switch(i) {
+      case 1:
+        break;
+    }
+    light.innerHTML = bootseq;
+  }
+  setTimeout(setTxt, 100);
 }
